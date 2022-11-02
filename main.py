84fafecd0fc600e9ae6c841e5752959d12241f93
texnic <kotya.karapetyan@gmail.com>
2022-11-02 23:14:15 +0100

    Added comments to Python code and cleaned up.

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
 def on_button_pressed_a():
-    DS3231.date_time(2022, 10, 30, 0, 20, 45, 0)
-    music.play_tone(262, music.beat(BeatFraction.QUARTER))
+    # Reset seconds.
+    # If current seconds value > 30, the minutes are increased by 1.
+    # If current seconds value <= 30, the minutes are left as is.
+    # NOTE: If minute == 59 and value > 30, an attempt will be made to set minute to 60.
+    # I am not sure what happens in this case.
+    if DS3231.second() > 30:
+        minute = DS3231.minute() + 1
+    else:
+        minute = DS3231.minute()
+    DS3231.date_time(DS3231.year(),
+        DS3231.month(),
+        DS3231.date(),
+        DS3231.day(),
+        DS3231.hour(),
+        minute,
+        0)
+    basic.show_number(DS3231.minute())
+    basic.show_number(DS3231.second())
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    # Show temperature_upper.
+    basic.show_number(DS3231.temperature_upper())
+    basic.pause(5000)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+s_old = 0
+s = 0
+minute2 = 0
+music.set_volume(32)
+
 def on_forever():
-    pass
+    global s, s_old
+    # Every minute, play a sound.
+    # Every 10 seconds, play another sound.
+    # Every second, play another sound.
+    # Try to show the value of seconds with an LED.
+    # With 25 LEDs, seconds between 0 and 24 are shown, then display
+    # goes dark until the end of the minute.
+    s = DS3231.second()
+    if s != s_old:
+        # if seconds changed, do something, else repeat
+        s_old = s
+        if s == 0:
+            music.play_tone(523, music.beat(BeatFraction.SIXTEENTH))
+        elif s % 10 == 0:
+            music.play_tone(523, music.beat(BeatFraction.HALF))
+        else:
+            music.play_tone(131, music.beat(BeatFraction.SIXTEENTH))
+        x = Math.floor(s / 5)
+        y = s % 5
+        basic.clear_screen()
+        led.plot(x, y)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,47 +1,55 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let minute: number;
+    //  Reset seconds.
+    //  If current seconds value > 30, the minutes are increased by 1.
+    //  If current seconds value <= 30, the minutes are left as is.
+    //  NOTE: If minute == 59 and value > 30, an attempt will be made to set minute to 60.
+    //  I am not sure what happens in this case.
     if (DS3231.second() > 30) {
         minute = DS3231.minute() + 1
     } else {
         minute = DS3231.minute()
     }
-    DS3231.dateTime(
-    DS3231.year(),
-    DS3231.month(),
-    DS3231.date(),
-    DS3231.day(),
-    DS3231.hour(),
-    minute,
-    0
-    )
+    
+    DS3231.dateTime(DS3231.year(), DS3231.month(), DS3231.date(), DS3231.day(), DS3231.hour(), minute, 0)
     basic.showNumber(DS3231.minute())
     basic.showNumber(DS3231.second())
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    //  Show temperature_upper.
     basic.showNumber(DS3231.temperatureUpper())
     basic.pause(5000)
 })
-let y = 0
-let x = 0
-let minute = 0
-let s = DS3231.second()
-let s_old = DS3231.second()
+let s_old = 0
+let s = 0
+let minute2 = 0
 music.setVolume(32)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    let x: number;
+    let y: number;
+    
+    //  Every minute, play a sound.
+    //  Every 10 seconds, play another sound.
+    //  Every second, play another sound.
+    //  Try to show the value of seconds with an LED.
+    //  With 25 LEDs, seconds between 0 and 24 are shown, then display
+    //  goes dark until the end of the minute.
     s = DS3231.second()
     if (s != s_old) {
+        //  if seconds changed, do something, else repeat
         s_old = s
         if (s == 0) {
             music.playTone(523, music.beat(BeatFraction.Sixteenth))
+        } else if (s % 10 == 0) {
+            music.playTone(523, music.beat(BeatFraction.Half))
         } else {
-            if (s % 10 == 0) {
-                music.playTone(523, music.beat(BeatFraction.Half))
-            } else {
-                music.playTone(131, music.beat(BeatFraction.Sixteenth))
-            }
+            music.playTone(131, music.beat(BeatFraction.Sixteenth))
         }
+        
         x = Math.floor(s / 5)
         y = s % 5
         basic.clearScreen()
         led.plot(x, y)
     }
+    
 })
